8a2e02f2503d5af44ca6ade2a156175237400bad
George Kemp <georgekemp94@gmail.com>
2021-01-26 14:02:21 -0700

    Update

--- lighthouse-service/user-data.sh
+++ lighthouse-service/user-data.sh
@@ -3,46 +3,39 @@
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 WORKING_DIR=/root/nauset/lighthouse-service
 
+# Update & Install Dependencies
 yum -y --security update
-
 yum -y update aws-cli
+yum -y install jq 
 
-yum -y install \
-  awslogs jq
+# Install & Launch Logs
+aws configure set default.region $REGION
+yum -y install awslogs
+cp -av $WORKING_DIR/awslogs.conf /etc/awslogs/
+sed -i "s|us-east-1|$REGION|g" /etc/awslogs/awscli.conf
+sed -i "s|%CLOUDWATCHLOGSGROUP%|$CLOUDWATCHLOGSGROUP|g" /etc/awslogs/awslogs.conf
+systemctl start awslogsd
 
-# Install Google Chrome
+# Install Chrome
 curl https://intoli.com/install-google-chrome.sh | bash
 
 # Install Node
 curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
-sudo yum install -y nodejs
+yum install -y nodejs
 
-# Install Lighthouse Globally
+# Install Lighthouse
 npm install lighthouse -g
 
-aws configure set default.region $REGION
+# mkdir /etc/init
+# cp -av $WORKING_DIR/convert-worker.conf /etc/init/convert-worker.conf
 
-# Copy Over Worker Services & Scripts to bin
-cp -av $WORKING_DIR/awslogs.conf /etc/awslogs/
-# cp -av $WORKING_DIR/spot-instance-interruption-notice-handler.conf /etc/init/spot-instance-interruption-notice-handler.conf
-mkdir /etc/init
-cp -av $WORKING_DIR/convert-worker.conf /etc/init/convert-worker.conf
-# cp -av $WORKING_DIR/spot-instance-interruption-notice-handler.sh /usr/local/bin/
-cp -av $WORKING_DIR/convert-worker.sh /usr/local/bin
+# Copy Bash Script to Bin
+cp -av $WORKING_DIR/lighthouse-worker.sh /usr/local/bin
 
-# chmod +x /usr/local/bin/spot-instance-interruption-notice-handler.sh
-chmod +x /usr/local/bin/convert-worker.sh
+chmod +x /usr/local/bin/lighthouse-worker.sh
 
 # Populate Worker Scripts w/ Variables
-sed -i "s|us-east-1|$REGION|g" /etc/awslogs/awscli.conf
-sed -i "s|%CLOUDWATCHLOGSGROUP%|$CLOUDWATCHLOGSGROUP|g" /etc/awslogs/awslogs.conf
-sed -i "s|%REGION%|$REGION|g" /usr/local/bin/convert-worker.sh
-sed -i "s|%S3BUCKET%|$S3BUCKET|g" /usr/local/bin/convert-worker.sh
-sed -i "s|%SQSQUEUE%|$SQSQUEUE|g" /usr/local/bin/convert-worker.sh
+sed -i "s|%REGION%|$REGION|g" /usr/local/bin/lighthouse-worker.sh
+sed -i "s|%SQSQUEUE%|$SQSQUEUE|g" /usr/local/bin/lighthouse-worker.sh
 
-# Start Logging Service
-systemctl start awslogsd
-# chkconfig awslogs on && service awslogs restart
-
-# start spot-instance-interruption-notice-handler
-start convert-worker
+start systemctl enable $WORKING_DIR/lighthouse.service
