cdacc9824e980f52ee2d50ced93df307afeab8aa
George Kemp <georgekemp94@gmail.com>
2021-01-26 11:08:36 -0700

    Initial Commit

---
+++ lighthouse-service/user-data.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+WORKING_DIR=/root/lighthouse-service
+
+yum -y --security update
+
+yum -y update aws-cli
+
+yum -y install \
+  awslogs jq
+
+# Install Google Chrome
+curl https://intoli.com/install-google-chrome.sh | bash
+
+# Install Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 14
+
+# Install Lighthouse Globally
+npm install lighthouse -g
+
+aws configure set default.region $REGION
+
+# Copy Over Worker Services & Scripts to bin
+cp -av $WORKING_DIR/awslogs.conf /etc/awslogs/
+# cp -av $WORKING_DIR/spot-instance-interruption-notice-handler.conf /etc/init/spot-instance-interruption-notice-handler.conf
+cp -av $WORKING_DIR/convert-worker.conf /etc/init/convert-worker.conf
+# cp -av $WORKING_DIR/spot-instance-interruption-notice-handler.sh /usr/local/bin/
+cp -av $WORKING_DIR/convert-worker.sh /usr/local/bin
+
+chmod +x /usr/local/bin/spot-instance-interruption-notice-handler.sh
+chmod +x /usr/local/bin/convert-worker.sh
+
+# Populate Worker Scripts w/ Variables
+sed -i "s|us-east-1|$REGION|g" /etc/awslogs/awscli.conf
+sed -i "s|%CLOUDWATCHLOGSGROUP%|$CLOUDWATCHLOGSGROUP|g" /etc/awslogs/awslogs.conf
+sed -i "s|%REGION%|$REGION|g" /usr/local/bin/convert-worker.sh
+sed -i "s|%S3BUCKET%|$S3BUCKET|g" /usr/local/bin/convert-worker.sh
+sed -i "s|%SQSQUEUE%|$SQSQUEUE|g" /usr/local/bin/convert-worker.sh
+
+chkconfig awslogs on && service awslogs restart
+
+# start spot-instance-interruption-notice-handler
+start convert-worker
